c042a760de58d09d467282fa6c4d0544639ae77f
yinzixu03 <34291825+yinzixu03@users.noreply.github.com>
2018-07-02 21:17:39 +0800

    Update gif.m

--- ws/gif.m
+++ ws/gif.m
@@ -2,7 +2,7 @@ clc;clear all;close all;
 filename='ws.gif';
 for i=1:231   %图片数
     I=imread(['D:\mantis\p2\' num2str(i) '.bmp']);
-    %[P,map]=rgb2ind(I,256,'dither');                %RGB图转换为索引图（制作GIF只能用2或者4波段数据）
+    %[P,map]=rgb2ind(I,256,'dither');   %RGB图转换为索引图（制作GIF只能用2或者4波段数据）
     if i==1;
         imwrite(P,map,filename,'gif','Loopcount',inf,'DelayTime',0.1);  %loopcount只是在i==1的时候才有用
     else
